d651431ed156e937c5878e405ca3831233d5b953
Adel Mohamed <adel.erorr159@gmail.com>
2025-01-23 12:28:24 +0200

    updateOOPTaskSession5

--- Session_P5/Duration.cs
+++ Session_P5/Duration.cs
@@ -2,22 +2,34 @@ namespace TaskSession_P5;
 
 public class Duration
 {
-    public double Minutes { get; set; }
-    public double Seconds { get; set; }
-    public double Hours { get; set; }
+    public int Minutes { get; set; }
+    public int Seconds { get; set; }
+    public int Hours { get; set; }
 
-    public Duration(double minutes, double seconds, double hours)
+    public Duration(int hours, int minutes, int seconds )
     {
         Minutes = minutes;
         Seconds = seconds;
         Hours = hours;
     }
 
-    public Duration(double NumberInSeconds)
+    public Duration(int NumberInSeconds)
     {
         Hours = NumberInSeconds / 3600;
         Minutes = (NumberInSeconds % 3600) / 60;
         Seconds = NumberInSeconds % 60;
+        
+        if (Seconds >= 60)
+        {
+            Minutes += Seconds / 60;
+            Seconds %= 60;
+        }
+
+        if (Minutes >= 60)
+        {
+            Hours += Minutes / 60;
+            Minutes %= 60;
+        }
     }
 
     public Duration()
@@ -26,7 +38,7 @@ public class Duration
     }
     public override string ToString()
     {
-        return $"Minutes = {Minutes}, Seconds = {Seconds}, Hours = {Hours}";
+        return $"Hours: {Hours}, Minutes: {Minutes}, Seconds: {Seconds}";
     }
 
     public override bool Equals(object? obj)
